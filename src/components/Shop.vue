7c8cb57519ad3b5461825881a39de1c22ab4b4ed
Jonathan Seidenschnur <jonathan.seidenschnur@ipmgroup.be>
2019-03-05 12:07:20 +0100

    add stripe

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,35 +1,20 @@
 <template>
-    <div>
-        <h1>Shop Moltin : order</h1>
-        <form>
-        <select name="user" @change="getCart($event)">  
-            <option></option>      
-            <option v-for="user in users" :key="user.id" :value="user.id">
-                {{user.name}}
-            </option>
-        </select>
-        <select name="product" @change="setCart($event)">    
-            <option></option>       
-            <option v-for="product in products" :key="product.id" :value="product.id">
-                {{product.name}}
-            </option>
-        </select>
-        <button @click.prevent="sendOrder()">Order</button></form><br/>
-        {{cart}}
-    <br/><br/>
-    {{order}}
-    <button @click.prevent="payOrder()">Paid</button><br/>
-    </div>
+    <div id='app'>
+    <h1>Please give us your payment details:</h1>
+    <card class='stripe-card'
+      :class='{ complete }'
+      stripe='pk_test_XXXXXXXXXXXXXXXXXXXXXXXX'
+      :options='stripeOptions'
+      @change='complete = $event.complete'
+    />
+    <button class='pay-with-stripe' @click='payOrder' :disabled='!complete'>Pay with credit card</button>
+  </div>
 </template>
 
 <script>
 import Axios from 'axios';
-import qs from 'qs';
-
-
-
-//const stripe = window.Stripe('pk_test_RbrEtpJVdCMKOSBHIZQ449Nd');
-
+//import { stripeKey, stripeOptions } from './stripeConfig.json'
+import { Card, createToken } from 'vue-stripe-elements-plus'
 
 
 export default {
@@ -37,7 +22,6 @@ export default {
   data() {
     return {
       products : [],
-      users : [],
       cart : null,
       order : null,
       api: {
@@ -45,164 +29,27 @@ export default {
           loaded : false
       },
       userId : null,
+      complete: false,
+      stripeOptions: {
+        // see https://stripe.com/docs/stripe.js#element-options for details
+      }
     }
   },
+  components: { Card },
   created() {
     this.loadDatas();
   },
   methods: {
       loadDatas() {
-          if (this.api.token === null) {
-              this.loadToken();
-              return false;
-          } 
-            /*const date = new Date(); const timestamp = date.getTime(); 
-            if(timestamp < this.api.token.expires) {
-                this.loadToken();
-                return false;
-            }*/
-            const axiosConfig = {
-                    method: 'GET',
-                    headers: {
-                    'Authorization': this.api.token.token_type+' ' + this.api.token.access_token,
-                    'Content-Type':'application/json'
-                    },
-                };
-            Axios.get(`https://api.moltin.com/v2/products`, axiosConfig)
-                .then((response) => {this.products = response.data.data})
-                .catch(function() {alert('no no 2')});
-            Axios.get(`https://api.moltin.com/v2/customers`, axiosConfig)
-                .then((response) => {this.users = response.data.data})
-                .catch(function() {alert('no no 2')});
-      },
-      loadToken() {
-          if (this.api.loaded) {
-              return false;
-          }
-          const axiosConfig = {
-                'content-type': 'application/x-www-form-urlencoded'
-            };
-          Axios.post('https://api.moltin.com/oauth/access_token',qs.stringify({
-              'client_id':'daLrAjMqgtViHIzs711vxUyBPtCCvM2Gw509xgxc97',
-              'client_secret':'Tv3nI639lxG5XfTazEJPSIwYBieLFGZfVkGpWvgQww',
-              'grant_type':'client_credentials'
-            }),axiosConfig)
-            .then((response) => {this.api.token = response.data; this.api.loaded = true})
-            .then(() => this.loadDatas())
-            .catch(function() {alert('no no 1')});
-      },
-      getCart(event) {
-          this.userId = event.target.value;
-          const axiosConfig = {
-                    method: 'GET',
-                    headers: {
-                    'Authorization': this.api.token.token_type+' ' + this.api.token.access_token,
-                    'Content-Type':'application/json'
-                    },
-                };
-            Axios.get(`https://api.moltin.com/v2/carts/`+event.target.value, axiosConfig)
-                .then((response) => {this.cart = response.data.data})
-                .catch(function() {alert('no no 2')});
-      },
-      setCart(event) {
-          if (this.cart === null) {
-              alert('non non');
-              return false;
-          }
-            const axiosConfig = {
-               headers: {
-                    'Authorization': this.api.token.token_type+' ' + this.api.token.access_token,
-                    'Content-Type':'application/json'
-                    },
-            };
-            Axios.post('https://api.moltin.com/v2/carts/'+this.cart.id+'/items',{data:{
-              'quantity':1,
-              'type':'cart_item',
-              'id':event.target.value
-            }},axiosConfig)
-            .then((response) => {})
-            .catch(function() {alert('no no 2')});
-      },
-      sendOrder() {
-          if (this.cart === null) {
-              alert('non non');
-              return false;
-          }
-            const axiosConfig = {
-               headers: {
-                    'Authorization': this.api.token.token_type+' ' + this.api.token.access_token,
-                    'Content-Type':'application/json'
-                    },
-            };
-            Axios.post('https://api.moltin.com/v2/carts/'+this.cart.id+'/checkout',{data:{
-                "customer": {
-                    "id": this.userId
-                },
-                "billing_address": {
-                    "first_name": "John",
-                    "last_name": "Doe",
-                    "company_name": "Moltin",
-                    "line_1": "2nd Floor British India House",
-                    "line_2": "15 Carliol Square",
-                    "city": "Newcastle upon Tyne",
-                    "postcode": "NE1 6UF",
-                    "county": "Tyne & Wear",
-                    "country": "UK"
-                },
-                "shipping_address": {
-                    "first_name": "John",
-                    "last_name": "Doe",
-                    "phone_number": "(555) 555-1234",
-                    "company_name": "Moltin",
-                    "line_1": "2nd Floor British India House",
-                    "line_2": "15 Carliol Square",
-                    "city": "Newcastle upon Tyne",
-                    "postcode": "NE1 6UF",
-                    "county": "Tyne & Wear",
-                    "country": "UK",
-                    "instructions": "Leave in porch"
-                }
-            }},axiosConfig)
-            .then((response) => {this.order = response.data.data;})
-            .catch(function() {alert('no no 2')});
+          
       },
       async payOrder() {
-
-          const data = {
-              type:'bancontact',
-              amount:this.order.meta.display_price.with_tax.amount,
-              currency:'eur',
-              owner:{
-                  name:'LOLLLLLL',
-                
-              },
-              redirect:{
-                  return_url:window.location.href
-              },
-              metadata:{
-                  order:this.order.id
-              }
-          }
-          const payload = await stripe.createSource(data);
-          console.log('source', payload);
-
-
-          const axiosConfig = {
-               headers: {
-                    'Authorization': this.api.token.token_type+' ' + this.api.token.access_token,
-                    'Content-Type':'application/json'
-                    },
-            };
-            Axios.post('https://api.moltin.com/v2/orders/'+this.order.id+'/payments',{data:{
-                "gateway": "stripe",
-                "method": "purchase",
-                "payment": payload.source.id,
-                "options": {
-                    "receipt_email": "jonathan.seidenschnur@ipmgroup.be"
-                }
-            }},axiosConfig)
-                .then((response) => {})
-                .catch(function() {alert('no no 2')});
+        // createToken returns a Promise which resolves in a result object with
+        // either a token or an error key.
+        // See https://stripe.com/docs/api#tokens for the token object.
+        // See https://stripe.com/docs/api#errors for the error object.
+        // More general https://stripe.com/docs/stripe.js#stripe-create-token.
+        createToken().then(data => console.log(data.token))
       }
   }
 }
